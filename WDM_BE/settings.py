03fc87ff0d97ee4891a3d6cfc0e7c71391e18037
Quy <frwk.HoangQuy@gmail.com>
2024-05-14 21:29:29 +0700

    [REFRACTOR] Thêm các app đã tạo vào file setting.py

--- WDM_BE/settings.py
+++ WDM_BE/settings.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'FoodService',
+    'Lobby',
+    'Login',
+    'Order',
+    'Report',
+    'User',
 ]
 
 MIDDLEWARE = [
