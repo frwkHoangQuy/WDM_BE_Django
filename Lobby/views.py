410614e3228b0093c242f17d79cbb9bf95444c29
Quy <frwk.HoangQuy@gmail.com>
2024-05-20 02:25:42 +0700

    [REFRACTOR] Sửa api get LobType sử dụng generics.ListApiView

--- Lobby/views.py
+++ Lobby/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import get_list_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 
 from .models import LobType, Lobby
 from .serializers import LobTypeSerializer
 
 
-class LobbyPageViews(APIView):
-
-    def get(self, request):
-        pass
-
-
-class LobbyTypeViews(LobbyPageViews):
-
-    def get(self, request):
-        lob_types = get_list_or_404(LobType)
-        serializer = LobTypeSerializer(lob_types, many=True)
-        return Response(serializer.data)
+class LobbyTypeViews(generics.ListAPIView):
+    queryset = LobType.objects.all()
+    serializer_class = LobTypeSerializer
