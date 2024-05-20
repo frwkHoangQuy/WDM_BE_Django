f9b84f1d0934323c028a487a0babedfd0a49123f
Quy <frwk.HoangQuy@gmail.com>
2024-05-20 14:29:08 +0700

    [FEATURE] Thêm api LobType SoftDelete

--- Lobby/serializers.py
+++ Lobby/serializers.py
@@ -7,16 +7,3 @@ class LobTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = LobType
         fields = '__all__'
-
-
-class LobTypeUpdateSerializer(serializers.ModelSerializer):
-    max_table_count = serializers.IntegerField(required=False)
-    min_table_price = serializers.IntegerField(required=False)
-    deposit_percent = serializers.IntegerField(required=False)
-    type_name = serializers.CharField()
-
-    class Meta:
-        model = LobType
-        field = ['max_table_count', 'min_table_price', 'deposit_percent', 'type_name']
-
-
--- Lobby/views.py
+++ Lobby/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import get_list_or_404
+from django.utils import timezone
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
-
+from rest_framework import status
 
 from .models import LobType, Lobby
 from .serializers import LobTypeSerializer
 
 
-class LobbyTypeViews(generics.ListAPIView):
+class LobTypeViews(generics.ListAPIView):
     queryset = LobType.objects.all()
     serializer_class = LobTypeSerializer
+
+
+class LobTypeUpdateViews(generics.UpdateAPIView):
+    queryset = LobType.objects.all()
+    serializer_class = LobTypeSerializer
+    lookup_field = 'id'
+
+
+class LobTypeSoftDeleteViews(generics.UpdateAPIView):
+    queryset = LobType.objects.all()
+    serializer_class = LobTypeSerializer
+    lookup_field = 'id'
+
+    def partial_update(self, request, **kwargs):
+        instance = self.get_object()
+        instance.deleted_at = timezone.now()
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
