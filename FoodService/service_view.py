3b026a0d5badbe7d0a699c82452d2b514bfc8a1a
Quy <frwk.HoangQuy@gmail.com>
2024-05-22 02:46:35 +0700

    [FEATURE] CRUD cho Service

---
+++ FoodService/service_view.py
@@ -0,0 +1,32 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from All_models.models import Service
+from Global_serializers.Service import ServiceSerializers
+
+
+class ServiceViews(generics.ListAPIView):
+    queryset = Service.objects.except_soft_delete()
+    serializer_class = ServiceSerializers
+
+
+class ServiceCreateViews(generics.CreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializers
+
+
+class ServiceEditViews(generics.UpdateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializers
+    lookup_field = 'id'
+
+
+class ServiceSoftDeleteViews(APIView):
+    def patch(self, request, id):
+        print(id)
+        soft_delete_service = get_object_or_404(Service, id=id)
+        soft_delete_service.soft_delete()
+        return Response({"detail": "Xóa thành công"}, status=status.HTTP_204_NO_CONTENT)
